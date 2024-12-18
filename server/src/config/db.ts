ac54a196146500d611bdddbf35415c47d81bb579
ruviRedner <r.y.r7966@gmail.com>
2024-12-18 12:38:28 +0200

    add log

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 export const connectDB = async () => {
     try {
         await connect(process.env.MONGODB_URI as string);
+        console.log("Connected to Mongo");
+        
     } catch (error) {
     }
 }
