d411f2bfeb5752e6cf348de251da3dc64f38b08a
ruviRedner <r.y.r7966@gmail.com>
2024-12-18 12:38:43 +0200

    empty files created

--- server/src/models/terrorModel.ts
+++ server/src/models/terrorModel.ts
@@ -1,23 +1,44 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
 const terrorSchema = new Schema({
-    iyear:{
-        type:Number,
-        required:true
-    },
-    imonth:{
-        type:Number,
-        required:true
-    },
-    region_txt:{
-        type:String,
-        required:true
-    },
-    latitude:{
-        type:Number,
-        required:true
-    },
-    
+  iyear: {
+    type: Number,
+    required: true,
+  },
+  imonth: {
+    type: Number,
+    required: true,
+  },
+  region_txt: {
+    type: String,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  attacktype1_txt: {
+    type: String,
+    required: true,
+  },
+  gname: {
+    type: String,
+    required: true,
+  },
+  nkill: {
+    type: Number,
+    required: true,
+  },
+  nwound: {
+    type: Number,
+    required: true,
+  },
+});
 
 
-})
+export const terrorModel = model("terror",terrorSchema)
+
---
+++ server/src/service/terror.service.ts
@@ -0,0 +1,1 @@
+import terrorList from "../data/globalterrorismdb_0718dist.json"
