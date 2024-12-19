e2e7926a72ed224ca02adf89eccc707b392beaad
ruviRedner <r.y.r7966@gmail.com>
2024-12-19 16:57:26 +0200

    add to schema

--- server/src/models/terrorModel.ts
+++ server/src/models/terrorModel.ts
@@ -3,40 +3,46 @@ import { model, Schema } from "mongoose";
 const terrorSchema = new Schema({
   iyear: {
     type: Number,
-    required: true,
+    
   },
   imonth: {
     type: Number,
-    required: true,
+    
   },
   region_txt: {
     type: String,
-    required: true,
+    
   },
   latitude: {
     type: Number,
-    required: true,
+  
   },
   longitude: {
     type: Number,
-    required: true,
+    
   },
   attacktype1_txt: {
     type: String,
-    required: true,
+    
   },
   gname: {
     type: String,
-    required: true,
+    
   },
   nkill: {
     type: Number,
-    required: true,
+    
   },
   nwound: {
     type: Number,
-    required: true,
+    
   },
+  city:{
+    type: String,
+  },
+  country_txt:{
+    type: String,
+  }
 });
 
 
