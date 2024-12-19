312beccee8e13a88148c7b1f9683f9e0122824f2
ruviRedner <r.y.r7966@gmail.com>
2024-12-19 14:28:42 +0200

    chack func if works

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,11 @@
-import express, { Request, Response } from 'express';
-import 'dotenv/config';
-import { connectDB } from './config/db';
-import cors from 'cors';
-import http from 'http';
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import { connectDB } from "./config/db";
+import cors from "cors";
+import http from "http";
 
-
-
-
-
-import 'dotenv/config';
+import "dotenv/config";
+import { getDeadliestRegionsByOrganization, getIncidentsInYearRange, getListAttackTypeByTheMostCasualties, getOganizationsWithTheMostIncidentByRegion, getRecentYearsData, getRegionWithTheHighestAverageCasualties, getTerroristGroupWithTheMostCasualties, getTop5OrganizationsWithTheMostIncidentByRegion, getUniceIncidentInEveryMonthInYear, seed } from "./service/terror.service";
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,12 +16,21 @@ app.use(cors());
 connectDB();
 app.use(express.json());
 
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send("pong");
+});
+// seed() 
+// getListAttackTypeByTheMostCasualties() 
+// getRegionWithTheHighestAverageCasualties()
+// getTerroristGroupWithTheMostCasualties(")
+// getUniceIncidentInEveryMonthInYear(1970)
+// getIncidentsInYearRange(2010,2020)
+// getRecentYearsData(10)
+// getTop5OrganizationsWithTheMostIncidentByRegion("East Asia")
+// getOganizationsWithTheMostIncidentByRegion("East Asia")
+// getDeadliestRegionsByOrganization("Unknown")
+server.listen(PORT, () =>
+  console.log(`Listening on port ${PORT},visit http://localhost:${PORT}`)
+);
 
 
-app.get('/ping', (req: Request, res: Response) => {
-    res.status(200).send('pong')});
-
-
-
-
-server.listen(PORT, () => console.log(`Listening on port ${PORT},visit http://localhost:${PORT}`));
