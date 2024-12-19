8dd32e9c0bb45848d5236215e97ac0c1d2c2b4ab
ruviRedner <r.y.r7966@gmail.com>
2024-12-19 19:53:18 +0200

    type in client

---
+++ client/src/types/newTerror.ts
@@ -0,0 +1,14 @@
+export interface Terror{
+    iyear: number;
+    imonth: number;
+    region_txt: string;
+    latitude: number;
+    longitude: number;
+    attacktype1_txt: string;
+    gname: string;
+    nkill: number;
+    nwound: number;
+    city:string;
+    country_txt: string;
+  
+  }
