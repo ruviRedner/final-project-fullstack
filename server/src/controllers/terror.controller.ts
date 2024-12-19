a53be46008d78b43ca135a1b50e660b93f83141e
ruviRedner <r.y.r7966@gmail.com>
2024-12-19 16:58:09 +0200

    controller for routes

---
+++ server/src/controllers/terror.controller.ts
@@ -0,0 +1,154 @@
+import { Request, Response } from "express";
+import {
+  getDeadliestRegionsByOrganization,
+  getIncidentsInYearRange,
+  getListAttackTypeByTheMostCasualties,
+  getOganizationsWithTheMostIncidentByRegion,
+  getOrganizationsOrIncidentsByYear,
+  getRecentYearsData,
+  getRegionWithTheHighestAverageCasualties,
+  getTop5OrganizationsWithTheMostIncidentByRegion,
+  getUniceIncidentInEveryMonthInYear,
+} from "../service/terror.service";
+
+export const fetchByType = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getListAttackTypeByTheMostCasualties();
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+
+export const fetchCasultysByType = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getRegionWithTheHighestAverageCasualties(
+      req.params.region
+    );
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+
+export const fetchCasultysByYear = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getUniceIncidentInEveryMonthInYear(req.params.year);
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+export const fetchCasultysByRangeYear = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getIncidentsInYearRange(
+      req.params.startYear,
+      req.params.endYear
+    );
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+export const fetchCasultysByRecentYear = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getRecentYearsData(req.params.range);
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+
+export const fetchIncidentTop5 = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getTop5OrganizationsWithTheMostIncidentByRegion(
+      req.params.region
+    );
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+
+export const fetchIncidentOfAllOrg = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getOganizationsWithTheMostIncidentByRegion(
+      req.params.region
+    );
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+
+export const fetchIncidentOfTheMostDhed = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const result = await getDeadliestRegionsByOrganization(
+      req.params.organizationName
+    );
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
+
+export const getOrganizationsOrIncidents = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { year, organizationName } = req.params;
+
+    if (!year) {
+      res.status(400).json({ error: "Year is required" });
+    }
+
+    const yearNumber = parseInt(year as string);
+
+    if (isNaN(yearNumber)) {
+      res.status(400).json({ error: "Invalid year format" });
+    }
+
+    const result = await getOrganizationsOrIncidentsByYear(
+      yearNumber,
+      organizationName as string
+    );
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: (error as Error).message });
+  }
+};
