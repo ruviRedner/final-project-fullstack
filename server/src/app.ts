9070675310821d80c30153376843481a463fd0b8
ruviRedner <r.y.r7966@gmail.com>
2024-12-19 17:57:55 +0200

    create socket

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,17 +3,25 @@ import "dotenv/config";
 import { connectDB } from "./config/db";
 import cors from "cors";
 import http from "http";
+import { Server } from "socket.io";
 
 import "dotenv/config";
 import { getDeadliestRegionsByOrganization, getIncidentsInYearRange, getListAttackTypeByTheMostCasualties, getOganizationsWithTheMostIncidentByRegion, getRecentYearsData, getRegionWithTheHighestAverageCasualties, getTerroristGroupWithTheMostCasualties, getTop5OrganizationsWithTheMostIncidentByRegion, getUniceIncidentInEveryMonthInYear, seed } from "./service/terror.service";
 import terrorRouter from "./routes/terror.router";
 import orgRouter from "./routes/org.router";
 import CreateRouter from "./routes/create.router";
+import { handelShackConnection } from "./socket/io";
 
 const PORT = process.env.PORT || 3000;
 
 export const app = express();
 export const server = http.createServer(app);
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
 
 app.use(cors());
 connectDB();
@@ -35,6 +43,7 @@ app.get("/ping", (req: Request, res: Response) => {
 // getTop5OrganizationsWithTheMostIncidentByRegion("East Asia")
 // getOganizationsWithTheMostIncidentByRegion("East Asia")
 // getDeadliestRegionsByOrganization("Unknown")
+io.on("connection", handelShackConnection);
 server.listen(PORT, () =>
   console.log(`Listening on port ${PORT},visit http://localhost:${PORT}`)
 );
