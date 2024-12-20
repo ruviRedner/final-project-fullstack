6d36c598531caf62a3ae0b817b2665e572326446
ruviRedner <r.y.r7966@gmail.com>
2024-12-20 09:18:57 +0200

    test socket in client success

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { io } from 'socket.io-client';
+import { TerrorResponce } from './types/responce';
 // import { TerrorResponce } from './types/responce';
 // import { Terror } from './types/newTerror';
 export const socket = io("http://localhost:9000");
@@ -24,6 +25,28 @@ const App:React.FC = () => {
   // socket.emit("delete",id,(res:TerrorResponce) => {
   //     console.log(res);
       
+  // })
+  // const id = "67641699ee80bd40a9d2c836"
+
+  // const update= {
+  //   imonth:7,
+  //   iyear:2021,
+  //   region_txt:"Middle East & North Africa",
+  //   latitude:32.4279,
+  //   longitude:53.6880,
+  //   attacktype1_txt:"Bombing/Explosion",
+  //   gname:"Hamas",
+  //   nkill:100000,
+  //   nwound:2,
+  //   city:"Abu Dhabi",
+  //   country_txt:"United Arab Emirates"
+  // }
+  // const data = {
+  //   id,
+  //   update,
+  // }
+  // socket.emit("update", data,(res:TerrorResponce)=> {
+  //   console.log(res)
   // })
   return (
     <div>
