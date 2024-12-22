43fa144496b263d19d3157238542b67ed0e395ae
ruviRedner <r.y.r7966@gmail.com>
2024-12-22 12:44:29 +0200

    change for nanege

--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Button from '@mui/material/Button';
 
-const Main:React.FC = () => {
+import "./main.css";
+import GraphAnimation from "../../components/graphs/GraphAnimation";
+import { useNavigate } from "react-router-dom";
+
+
+const Main: React.FC = () => {
+  const navigate= useNavigate()
+  const handelGraph = () => {
+      navigate("/graph-manege")
+  }
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="main">
+        <h1>ברוכים הבאים לאתר ניהול אירועי טרור עולמיים על ידי מפות וגרפים</h1>
+        <GraphAnimation />
+        <div className="btn">
+        <Button variant="contained">הצג מפה </Button>
+        <Button onClick={handelGraph} variant="contained">הצג גרף</Button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default :React.FC
+export default Main;
