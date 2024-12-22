84a7c0a87bece79f9f62ab906a69bc599060a088
ruviRedner <r.y.r7966@gmail.com>
2024-12-22 16:20:36 +0200

    add socket

--- server/src/socket/io.ts
+++ server/src/socket/io.ts
@@ -1,8 +1,15 @@
 import { Socket } from "socket.io";
-import { io } from "../app";
 import { Terror } from "../models/terrorModel";
-import { createNewEvent, deleteEvent, updataEvent } from "../service/create.service";
-import { getListAttackTypeByTheMostCasualties, getRegionWithTheHighestAverageCasualties } from "../service/terror.service";
+import {
+  createNewEvent,
+  deleteEvent,
+  updataEvent,
+} from "../service/create.service";
+import {
+  getIncidentsInYearRange,
+  getListAttackTypeByTheMostCasualties,
+  getUniceIncidentInEveryMonthInYear,
+} from "../service/terror.service";
 
 export const handelShackConnection = (client: Socket) => {
   console.log(`[socket.io]New Connection ${client.id} `);
@@ -31,18 +38,39 @@ export const handelShackConnection = (client: Socket) => {
       callback({ success: false, message: "event deleted went wrong" });
     }
   });
-  client.on("update", async (data: { id: string; update: Terror }, callback) => {
-    try {
-      const updated = await updataEvent(data.id, data.update);
-      callback({ success: true, message: updated });
-    } catch (error) {
-      callback({ success: false, message: "event updated went wrong" });
+  client.on(
+    "update",
+    async (data: { id: string; update: Terror }, callback) => {
+      try {
+        const updated = await updataEvent(data.id, data.update);
+        callback({ success: true, message: updated });
+      } catch (error) {
+        callback({ success: false, message: "event updated went wrong" });
+      }
     }
-  })
-  client.on("get1",async (callback) => {
+  );
+  client.on("get1", async (callback) => {
     try {
-      const result = await getListAttackTypeByTheMostCasualties(
-      );
+      const result = await getListAttackTypeByTheMostCasualties();
+      callback({ success: true, data: result });
+    } catch (error) {
+      callback({ success: false, message: (error as Error).message });
+    }
+  });
+  client.on("get2", async (year:string, callback) => {
+    try {
+      
+      const result = await getUniceIncidentInEveryMonthInYear(year);
+      
+      callback({ success: true, data: result });
+    } catch (error) {
+      callback({ success: false, message: (error as Error).message });
+    }
+  });
+  client.on("get3", async (data:{startYear:string,endYear:string}, callback) => {
+    try {
+      const result = await getIncidentsInYearRange(data.startYear,data.endYear);
+      
       callback({ success: true, data: result });
     } catch (error) {
       callback({ success: false, message: (error as Error).message });
