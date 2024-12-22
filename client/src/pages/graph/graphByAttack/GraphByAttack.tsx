5d54c80ca9845d0296d2a0f23a47263bd31c1c61
ruviRedner <r.y.r7966@gmail.com>
2024-12-22 16:17:43 +0200

    change import location

--- client/src/pages/graph/graphByAttack/GraphByAttack.tsx
+++ client/src/pages/graph/graphByAttack/GraphByAttack.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { socket } from "../../../App";
 import { TerrorResponce } from "../../../types/responce";
-import "./graph.css";
+import "../graph.css";
 import { Get1Type } from "../../../types/typeFor1";
 
 const GraphByAttack: React.FC = () => {
@@ -11,12 +11,11 @@ const GraphByAttack: React.FC = () => {
   useEffect(() => {
     socket.emit("get1", (res: TerrorResponce) => {
       setData(res.data);
-      console.log(res.data);
     });
   }, []);
 
-  const xAxisData = data.map((item) => item.attackType);
-  const seriesData = [{ data: data.map((item) => item.totalCasualties) }];
+  const xAxisData = data?.map((item) => item.attackType);
+  const seriesData = [{ data: data?.map((item) => item.totalCasualties) }];
 
   return (
     <div className="container">
