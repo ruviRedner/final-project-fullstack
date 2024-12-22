47967fb24d334526e4ddf62cfc398191067b8ccd
ruviRedner <r.y.r7966@gmail.com>
2024-12-22 23:32:10 +0200

    type and array for select

---
+++ client/src/types/typeFor5.ts
@@ -0,0 +1,43 @@
+export interface GetType5{
+    incident: number
+    orgName: string       
+    lat:number
+    lng:number
+}
+
+export const regions = [
+    {
+        name:"Australasia & Oceania"
+    },
+    {
+        name:"Western Europe"
+    },
+    {
+        name:"South America"
+    },
+    {
+        name:"Southeast Asia"
+    },
+    {
+        name:"Central America & Caribbean"
+    },{
+        name:"Eastern Europe"
+    },
+    {
+        name:"Central Asia"
+    },
+    {
+        name:"South Asia"
+    },{
+        name:"Sub-Saharan Africa"
+    },
+    {
+        name:"North America"
+    },
+    {
+        name:"Middle East & North Africa"
+    },
+    {
+        name:"East Asia"
+    }
+]
