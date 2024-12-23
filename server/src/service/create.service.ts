12a1976794266e42d92a582287d084cb0104c19d
ruviRedner <r.y.r7966@gmail.com>
2024-12-23 22:18:31 +0200

    add func for get org names and search in text

--- server/src/service/create.service.ts
+++ server/src/service/create.service.ts
@@ -31,44 +31,64 @@ export const createNewEvent = async (newEvent: Terror) => {
     ) {
       throw new Error("Invalid data");
     }
-    const data = await terrorModel.create(newEvent)
+    const data = await terrorModel.create(newEvent);
     return data;
   } catch (error) {
     return handleBadRequest("Invalid data", error);
   }
 };
 
-export const deleteEvent = async (id: string)=> {
+export const deleteEvent = async (id: string) => {
   try {
-    if(!id) {
+    if (!id) {
       throw new Error("ID is requierd");
     }
     const findId = terrorModel.findById(id);
-    if(!findId) {
+    if (!findId) {
       throw new Error("ID not found");
     }
     await terrorModel.deleteOne(findId);
-    return {message: "Event deleted successfully"};
-    
+    return { message: "Event deleted successfully" };
   } catch (error) {
     return handleBadRequest("Failed to delete event", error);
-    
   }
-}
-export const updataEvent= async (id:string,update:Terror) => {
+};
+export const updataEvent = async (id: string, update: Terror) => {
   try {
-    if(!id) {
+    if (!id) {
       throw new Error("ID is required");
     }
-    const findId = await terrorModel.findByIdAndUpdate(id,{$set:  update}, {new: true});
-    if(!findId) {
+    const findId = await terrorModel.findByIdAndUpdate(
+      id,
+      { $set: update },
+      { new: true }
+    );
+    if (!findId) {
       throw new Error("ID not found");
     }
     return findId;
-   
-    
   } catch (error) {
-    return handleBadRequest("Bad Request",error)
-    
+    return handleBadRequest("Bad Request", error);
   }
-}
+};
+
+export const getOrgNames = async () => {
+  try {
+    const data = await terrorModel.distinct("gname");
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    return handleBadRequest("Bad request", error);
+  }
+};
+export const searchInText = async (str: string) => {
+  try {
+    const regex = new RegExp(str, "i");
+    const result = await terrorModel.find({ summary: regex }).limit(30);
+
+    return result;
+  } catch (error) {
+    return handleBadRequest("Bad request", error);
+  }
+};
