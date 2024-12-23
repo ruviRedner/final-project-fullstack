34cfea5a1b636a736cdb6986d03da03417733f06
ruviRedner <r.y.r7966@gmail.com>
2024-12-23 22:14:37 +0200

    added condition if empty array

--- client/src/pages/graph/graphByAttack/GraphByAttack.tsx
+++ client/src/pages/graph/graphByAttack/GraphByAttack.tsx
@@ -7,9 +7,15 @@ import { Get1Type } from "../../../types/typeFor1";
 
 const GraphByAttack: React.FC = () => {
   const [data, setData] = useState<Get1Type[]>([]);
+  const [isData, setIsData] = useState(true);
 
   useEffect(() => {
     socket.emit("get1", (res: TerrorResponce) => {
+      if(res.data.length <= 0) {
+        setIsData(false);
+        return;
+      }
+      setIsData(true);
       setData(res.data);
     });
   }, []);
@@ -19,6 +25,7 @@ const GraphByAttack: React.FC = () => {
 
   return (
     <div className="container">
+      {!isData && <h3>אין נתונים להצגה</h3>}
       <h2>מידע על כמות נפגעים לפי תקיפות</h2>
       <div className="animation">
         <BarChart
