12387145198ae72c58c4b82115e2366908b91a05
ruviRedner <r.y.r7966@gmail.com>
2024-12-23 22:16:19 +0200

    add map animation

--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,25 +1,41 @@
 import React from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 import "./main.css";
 import GraphAnimation from "../../components/graphs/GraphAnimation";
 import { useNavigate } from "react-router-dom";
-
+import MapsAnimation from "../../components/maps/MapsAnimation";
 
 const Main: React.FC = () => {
-  const navigate= useNavigate()
+  const navigate = useNavigate();
   const handelGraph = () => {
-      navigate("/graph-manege")
-  }
+    navigate("/graph-manege");
+  };
+  const handelMap = () => {
+    navigate("/map-1");
+  };
   return (
     <>
       <div className="main">
         <h1>ברוכים הבאים לאתר ניהול אירועי טרור עולמיים על ידי מפות וגרפים</h1>
-        <GraphAnimation />
-        <div className="btn">
-        <Button variant="contained">הצג מפה </Button>
-        <Button onClick={handelGraph} variant="contained">הצג גרף</Button>
+        <div className="animation">
+          <div className="gra">
+            <GraphAnimation />
+          </div>
+          <div className="map">
+            <MapsAnimation />
+          </div>
+          <div className="btn">
+          <Button onClick={handelGraph} variant="contained">
+            הצג גרף
+          </Button>
+          <Button onClick={handelMap} variant="contained">
+            הצג מפה{" "}
+          </Button>
         </div>
+        </div>
+
+        
       </div>
     </>
   );
