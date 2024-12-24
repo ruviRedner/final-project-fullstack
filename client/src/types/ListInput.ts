4660a763de4eb1046dbf0aca7e436d65a278df25
ruviRedner <r.y.r7966@gmail.com>
2024-12-24 16:47:19 +0200

    list inputs

---
+++ client/src/types/ListInput.ts
@@ -0,0 +1,42 @@
+export const InputList = [
+  {
+    id: "imonth",
+    display: "month",
+  },
+  {
+    id: "iyear",
+    display: "year",
+  },
+  {
+    id: "region_txt",
+    display: "region",
+  },
+  {
+    id: "attacktype1_txt",
+    display: "attacktype",
+  },
+  {
+    id: "gname",
+    display: "org name",
+  },
+  {
+    id: "nkill",
+    display: "kill",
+  },
+  {
+    id: "nwound",
+    display: "wound",
+  },
+  {
+    id: "city",
+    display: "city",
+  },
+  {
+    id: "country_txt",
+    display: "country",
+  },
+  {
+    id: "summary",
+    display: "description",
+  },
+];
