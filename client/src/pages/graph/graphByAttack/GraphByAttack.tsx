4764c61a728d30025acc70b682bb63a6879e1075
ruviRedner <r.y.r7966@gmail.com>
2024-12-24 16:46:11 +0200

    added real time socket

--- client/src/pages/graph/graphByAttack/GraphByAttack.tsx
+++ client/src/pages/graph/graphByAttack/GraphByAttack.tsx
@@ -8,17 +8,32 @@ import { Get1Type } from "../../../types/typeFor1";
 const GraphByAttack: React.FC = () => {
   const [data, setData] = useState<Get1Type[]>([]);
   const [isData, setIsData] = useState(true);
+  const [dataChanged, setDataChanged] = useState<boolean>(false)
+  useEffect(() => {
+    socket.on("change-data", (data: { action: string; data: any }) => {
+        if (data.action === "create") {
+          setDataChanged(true)
+        } else if (data.action === "update") {
+          setDataChanged(true)
+        } else if (data.action === "delete") {
+          setDataChanged(true)
+        }
+    });
 
+    return () => {
+      socket.off("change-data");
+    };
+  }, []);
   useEffect(() => {
     socket.emit("get1", (res: TerrorResponce) => {
-      if(res.data.length <= 0) {
+      if (res.data.length <= 0) {
         setIsData(false);
         return;
       }
       setIsData(true);
       setData(res.data);
     });
-  }, []);
+  }, [dataChanged]);
 
   const xAxisData = data?.map((item) => item.attackType);
   const seriesData = [{ data: data?.map((item) => item.totalCasualties) }];
