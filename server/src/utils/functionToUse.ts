3625eec2450ff65affed6883670012763c7ee1a2
ruviRedner <r.y.r7966@gmail.com>
2024-12-24 16:50:49 +0200

    delete unused import

--- server/src/utils/functionToUse.ts
+++ server/src/utils/functionToUse.ts
@@ -1,5 +1,4 @@
 import fs from 'fs/promises';
-import { EventData } from '../service/terror.service';
 export const getFileData = async  (resourse:string) => {
     try {
         const data:string = (await fs.readFile(`${__dirname}/../../../../data/${resourse}.json`, 'utf-8')).toString();
