0de65d25128614216703167c28da2c60b97e765c
ruviRedner <r.y.r7966@gmail.com>
2024-12-24 16:47:35 +0200

    fix letters

--- client/src/types/newTerror.ts
+++ client/src/types/newTerror.ts
@@ -2,13 +2,13 @@ export interface Terror{
     iyear: number;
     imonth: number;
     region_txt: string;
-    latitude: number;
-    longitude: number;
     attacktype1_txt: string;
     gname: string;
     nkill: number;
     nwound: number;
     city:string;
     country_txt: string;
+    latitube: number;
+    longitude: number;
   
   }
