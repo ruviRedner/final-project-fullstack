513be0c61f3cec2b8c0e2a7dca0859f509efccd9
ruviRedner <r.y.r7966@gmail.com>
2024-12-24 16:49:38 +0200

    changing to number

--- server/src/models/terrorModel.ts
+++ server/src/models/terrorModel.ts
@@ -1,75 +1,65 @@
 import { model, Schema } from "mongoose";
-export interface Terror{
+export interface Terror {
   iyear: number;
   imonth: number;
   region_txt: string;
-  latitude: number;
-  longitude: number;
   attacktype1_txt: string;
   gname: string;
   nkill: number;
   nwound: number;
-  city:string;
+  city: string;
   country_txt: string;
   summary: string;
-
+  latitube: number;
+  longitude: number;
 }
 const terrorSchema = new Schema({
   iyear: {
     type: Number,
     index: true,
-    
   },
   imonth: {
     type: Number,
-    
   },
   region_txt: {
     type: String,
-    index:true
-    
+    index: true,
   },
-  latitude: {
-    type: Number,
-  
-  },
-  longitude: {
-    type: Number,
-    
-  },
+
   attacktype1_txt: {
     type: String,
-    
   },
   gname: {
     type: String,
-    index:true
-    
+    index: true,
   },
   nkill: {
     type: Number,
-    
   },
   nwound: {
     type: Number,
-    
   },
-  city:{
+  city: {
     type: String,
-    index:true,
+    index: true,
   },
-  country_txt:{ 
+  country_txt: {
     type: String,
   },
-  summary:{
+  summary: {
     type: String,
-    index:true,
-  }
+    index: true,
+  },
+  latitude: {
+    type: Number,
+  },
+  longitude: {
+    type: Number,
+  },
 });
 
-terrorSchema.index({gname:1})
+terrorSchema.index({ gname: 1 });
 
 terrorSchema.index({ nkill: 1, nwound: 1 });
 terrorSchema.index({ city: 1, gname: 1 });
-export const terrorModel = model("terror",terrorSchema)
-
+export const terrorModel = model("terror", terrorSchema);
